865f8bd35c06a9d016494e1a367a23665190aede
Karn Wong <kahnwong@Karns-MacBook-Air.local>
2017-06-01 12:06:27 +0700

    Add support for The Die Line and Popculturenexus

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # gallery-ripper
-A short Python script to download images from Bored Panda, Fubiz and Kotaku
+A short Python script to download images from Bored Panda, Fubiz Kotaku, Popculturenexus and The Die Line
 ----
 Paste urls in ```urls.txt``` and run ```gallery_dl.py``` to begin download.
+
+## Update June 1, 2017
+
+Add support for The Die Line and Popculturenexus
 
 ## Dependencies
 
--- gallery_dl.py
+++ gallery_dl.py
@@ -2,6 +2,7 @@ import os
 from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 # import better_exceptions
 
 class Scraper(object):
@@ -9,9 +10,9 @@ class Scraper(object):
         self.url = url
 
     def make_request(self):
-        self.response = requests.get(self.url, allow_redirects=True).content
-        self.soup = BeautifulSoup(self.response, 'html.parser')
-        return self.soup
+        response = requests.get(self.url, allow_redirects=True).content
+        soup = BeautifulSoup(response, 'html.parser')
+        return soup
 
     def get_images(self):
         raise NotImplementedError
@@ -28,21 +29,28 @@ class Scraper(object):
         images = self.get_images()
         print(images)
 
-        for image in images:
+        for index, image in enumerate(images):
             parsed = urlparse(image)
             filename = parsed.path.split('/')[-1]
-
             image_b = requests.get(image)
+
+            if '.jpg' not in filename:
+                filename = parsed.path.split('/')[-2] + '.jpg'
+
+
+            """no enum"""
             with open(album + '/' + filename, 'wb') as img_obj:
                 img_obj.write(image_b.content)
             print(filename)
             # break #test
         print('-----------------------------------')
+        sleep(3)
 
 
 class Fubiz(Scraper):
     def get_thumbnail(self):
-        for link in self.soup.find_all("a", class_="lightbox"):
+        soup = self.make_request()
+        for link in soup.find_all("a", class_="lightbox"):
             thumbnail = link.get('href') # return image wrapper url
             break
 
@@ -50,7 +58,7 @@ class Fubiz(Scraper):
             print(thumbnail)
             self.url = thumbnail
         except UnboundLocalError:
-            for scrape in self.soup.find_all('div', class_='inner-post-content'):
+            for scrape in soup.find_all('div', class_='inner-post-content'):
                 for link in scrape.find_all('a'):
                     continue
                 thumbnail = link.get('href')
@@ -61,7 +69,7 @@ class Fubiz(Scraper):
 
 
     def get_images(self):
-        soup = super().make_request()
+        soup = self.make_request()
         images = [link.get('src') for link in soup.select('div > img')]
         return images
 
@@ -72,8 +80,9 @@ class Fubiz(Scraper):
 
 class Kotaku(Scraper):
     def get_images(self):
+        soup = self.make_request()
         images = []
-        for image in self.soup.select('img[data-chomp-id]'):
+        for image in soup.select('img[data-chomp-id]'):
             image_id = image.get('data-chomp-id')
             extension = image.get('data-format')
             images.append(image_id + '.' + extension)
@@ -88,14 +97,15 @@ class Kotaku(Scraper):
 
 class Boredpanda(Scraper):
     def get_images(self):
+        soup = self.make_request()
         images = []
-        for link in self.soup.select('div > p > img'):
+        for link in soup.select('div > p > img'):
             src = link.get('src')
             images.append(src)
 
         # if len(images) == 0:
         if not images:
-            for link in self.soup.select('div > p > span > img'):
+            for link in soup.select('div > p > span > img'):
                 src = link.get('src')
                 images.append(src)
         return images
@@ -105,6 +115,42 @@ class Boredpanda(Scraper):
         album = parsed.path.replace('-', ' ').title().replace('/', '')
         return album
 
+class Popculturenexus(Scraper):
+    def get_images(self):
+        soup = self.make_request()
+        images = []
+        for link in soup.select('p > a > img'):
+            try:
+                image = link.get('data-orig-file').split('?')[0]
+                images.append(image)
+                # print(image)
+            except AttributeError:
+                pass
+
+        return images
+
+    def album_name(self):
+        parsed = urlparse(self.url)
+        album = parsed.path.split('/')[4].replace('-', ' ').title()
+        return album
+
+class Thedieline(Scraper):
+    def get_images(self):
+        soup = self.make_request()
+        images = []
+        for scrape in soup.find_all(class_='sqs-layout sqs-grid-12 columns-12'):
+            for link in scrape.find_all('img'):
+                img = link.get('src')
+                print(img)
+                images.append(img)
+
+        images = [img for img in images if img]
+        return images
+
+    def album_name(self):
+        parsed = urlparse(self.url)
+        album = parsed.path.split('/')[-1].replace('-', ' ').title()
+        return album
 
 
 def main(filename):
@@ -115,17 +161,25 @@ def main(filename):
     for site in sites:
         if 'fubiz' in site:
             i = Fubiz(site)
-            i.make_request()
             i.get_thumbnail()
             i.download()
         elif 'kotaku' in site:
             i = Kotaku(site)
-            i.make_request()
             i.download()
 
         elif 'boredpanda' in site:
             i = Boredpanda(site)
-            i.make_request()
+            i.download()
+
+        elif 'popculturenexus' in site:
+            i = Popculturenexus(site)
+            i.download()
+
+        elif 'thedieline' in site:
+            i = Thedieline(site)
             i.download()
 
 main('urls.txt')
+
+# """enum"""
+# with open(album + '/' + str(index) + ' ' + filename, 'wb') as img_obj:
